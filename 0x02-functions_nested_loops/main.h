93a234e39cfdf3b5e546918dc4ebc3213148c000
edwin uzoma <fuzibibi@gmail.com>
2022-10-18 14:47:48 -0700

    working directory

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,12 +5,12 @@
 *
 * Return: 0.
 */
-int main(void)  
-{
+void main(void);
+/**
 char word[9] = "main";
 int i;
 for (i = 0; i < 9; i++)
 _putchar(word[i]);
 int_putchar('\n');
-*Return : 0
+*Return: 0
 #endif
