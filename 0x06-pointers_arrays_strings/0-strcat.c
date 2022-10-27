2238a65091458a034ac6cd254de7ff2aaaa826bb
edwin uzoma <fuzibibi@gmail.com>
2022-10-27 09:31:48 -0700

    my personal work

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *n);
-char *cap_string(char *n);
-char *leet(char *n);
-char *rot13(char *n);
-#endif /* MAIN_H */
+#include "main.h"
+/**
+* _strcat - a function that concatenates two strings
+* @dest: copy to
+* @src: copy from
+* Return: two concat strings
+*/
+char *_strcat(char *dest, char *src)
+{
+int i, n;
+for (i = 0; dest[i] != '\0'; i++)
+{
+}
+for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+{
+}
+return (dest);
+}
