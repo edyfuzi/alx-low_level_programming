b06ca725af3725b9b034434b387f643fe31bb45d
edwin uzoma <fuzibibi@gmail.com>
2022-11-15 12:19:32 -0800

    my personal work

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,9 +8,9 @@
 void print_dog(struct dog *d)
 {
 if (d)
-{      
+{
 if (d->name)
-printf("Name: %s\n", d->name);      
+printf("Name: %s\n", d->name);
 else
 printf("Name: (nil)\n");
 if (d->age >= 0)
